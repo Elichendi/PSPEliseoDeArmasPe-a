ae190ac4bc670f09663b2dd2e77ee70a162d8e69
Elichendi <167315427+Elichendi@users.noreply.github.com>
2026-09-24 13:07:14 +0200

    Add files via upload

---
+++ UT1/fork01.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void main() 
+{
+    pid_t pid, pid_hijo, pid_padre;
+    pid = fork();
+
+    if (pid==0)
+    {
+        printf("Soy el proceso hijo \n");
+        printf("Mi PID es: %d\n",getpid());
+        printf("El PID de mi padre es: %d\n",getppid());
+        pid_hijo=getpid();
+    }
+
+    else
+    {
+        pid_hijo = wait(NULL);
+        printf("Soy el proceso padre \n");
+        printf("Mi PID es: %d\n",getpid());
+        printf("El PID de mi hijo era: %d\n",pid_hijo);
+        printf("El proceso hijo ha terminado");
+    }
+}
